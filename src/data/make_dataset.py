4246bf4666915d6b7a97daf2693bc8803979647c
leo.ferretti <leo.ferretti@epita.fr>
2023-07-04 21:15:11 +0200

    modify flake8

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,22 @@
 import pickle
 import pandas as pd
 
+
+path = "data/raw/cifar-10-batches-py/"
+
+
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-path = "data/raw/cifar-10-batches-py/"
-
 
 # Load the data
-
-for data_batch in ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]:
+for data_batch in ["data_batch_1",
+                   "data_batch_2",
+                   "data_batch_3",
+                   "data_batch_4",
+                   "data_batch_5"]:
     batch = unpickle(path + data_batch)
     data = batch[b'data']
     labels = batch[b'labels']
@@ -20,7 +25,6 @@ for data_batch in ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4
 df_train = pd.DataFrame(data)
 df_train['label'] = labels
 df_train.to_csv('data/processed/train.csv', index=False)
-
 
 
 test_batch = unpickle(path + "test_batch")
